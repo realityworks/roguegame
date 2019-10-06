2806727270fae2aef1daa6dcff8145e19267c505
Peter Suwara <psuwara@realityworks.com.au>
2019-10-06 20:41:11 +0300

    Started implementing spells

--- item_functions.py
+++ item_functions.py
@@ -15,3 +15,18 @@ def heal(*args, **kwargs):
         entity.fighter.heal(amount)
         results.append({'consumed': True, 'message': Message('Your wounds start to heal', libtcod.green)})
     return results
+
+def cast_lightning(*args, **kwargs):
+    caster = args[0]
+    entities = kwargs.get('entities')
+    fov_map = kwargs.get('fov_map')
+    damage = kwargs.get('damage')
+    maximum_range = kwargs.get('maximum_range')
+
+    results = []
+
+    target = None
+    closest_distance = maximum_range + 1
+
+    for entity in entities:
+
